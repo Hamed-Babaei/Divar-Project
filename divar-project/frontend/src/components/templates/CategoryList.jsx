30ce06cbc1a012858d9a7edd544ec87b2a7a3301
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-02-13 02:44:27 +0330

    fix(getCategories): fix bug in get all categories

--- divar-project/frontend/src/components/templates/CategoryList.jsx
+++ divar-project/frontend/src/components/templates/CategoryList.jsx
@@ -1,10 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import { getCategory } from "src/services/admin";
 import Loader from "../modules/Loader/Loader";
 
 const CategoryList = () => {
-  const { data, isLoading } = useQuery(["get-categories"], getCategory);
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery(["get-categories"], getCategory, {
+    onSuccess: () => queryClient.invalidateQueries("get-categories"),
+  });
 
   const deleteHandler = (slug) => {
     // e.preventDefault();
@@ -17,41 +20,36 @@ const CategoryList = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        data.data.map((item, index) => (
-          <table
-            key={index}
-            className="min-w-full bg-white border border-gray-300"
-          >
-            <thead>
-              <tr>
-                <th className="py-2 text-start px-4 border-b">نام کتگوری</th>
-                <th className="py-2 text-start px-4 border-b">اسلاگ</th>
-                <th className="py-2 text-start px-4 border-b">آیکون</th>
-                <th className="py-2 text-start px-4 border-b"> </th>
+        <table className="min-w-full bg-white border border-gray-300">
+          <thead>
+            <tr>
+              <th className="py-2 text-start px-4 border-b">نام کتگوری</th>
+              <th className="py-2 text-start px-4 border-b">اسلاگ</th>
+              <th className="py-2 text-start px-4 border-b">آیکون</th>
+              <th className="py-2 text-start px-4 border-b"> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.data.map((item, index) => (
+              <tr key={index}>
+                <td className="py-2 px-4 border-b">{item.name}</td>
+                <td className="py-2 px-4 border-b">{item.slug}</td>
+                <td className="py-2 px-4 border-b">
+                  <img src={`${item.icon}.svg`} />
+                </td>
+                <td className="py-2 px-4 border-b">
+                  <button
+                    className="bg-[#a62626] text-white border-none px-[8px] py-[5px] rounded-md text-sm cursor-pointer disabled:opacity-50 hover:opacity-85 duration-200"
+                    onClick={() => deleteHandler(item.slug)}
+                    y
+                  >
+                    حذف
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {data.data.map((item, index) => (
-                <tr key={index}>
-                  <td className="py-2 px-4 border-b">{item.name}</td>
-                  <td className="py-2 px-4 border-b">{item.slug}</td>
-                  <td className="py-2 px-4 border-b">
-                    <img src={`${item.icon}.svg`} />
-                  </td>
-                  <td className="py-2 px-4 border-b">
-                    <button
-                      className="bg-[#a62626] text-white border-none px-[8px] py-[5px] rounded-md text-sm cursor-pointer disabled:opacity-50 hover:opacity-85 duration-200"
-                      onClick={() => deleteHandler(item.slug)}
-                    >
-                      حذف
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-            s
-          </table>
-        ))
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
